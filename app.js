b5f368d6a560ba5c51eccfd1c0a3906dbb7a3a3d
unknown <markbowman100@gmail.com>
2020-05-04 08:06:04 -0400

    Added quick sort

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var arrayOfInt = [
     9,8,7,6,5,4,3,2,1
 ];
 
-console.log(arrayOfInt);
 sort.insertionSort(arrayOfInt.length, arrayOfInt)
-console.log(arrayOfInt);
+
+var arrayOfInt2 = [
+    9,8,7,6,5,4,3,2,1,9
+];
+
+sort.quickSort(arrayOfInt2, 0, 9)
+console.log(arrayOfInt2)
--- sort.js
+++ sort.js
@@ -16,6 +16,44 @@ class Sort {
             array[a - 1] = key;
         }  
     }
+
+    partition(arr, low, high) { 
+        var pivot = arr[high];  
+        var i = low;
+        for (var j = low; j < high; j++) { 
+            if (arr[j] < pivot) { 
+                var temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp; 
+                i++;
+            } 
+            else {
+                //Do nothing
+            }
+        } 
+  
+        var temp = arr[i]; 
+        arr[i] = arr[high]; 
+        arr[high] = temp; 
+  
+        return i; 
+    } 
+
+    quickSort(arr, low, high) { 
+        if (low < high) { 
+            /* pi is partitioning index, arr[pi] is  
+              now at right place */
+            var pivot = this.partition(arr, low, high); 
+  
+            // Recursively sort elements before 
+            // partition and after partition 
+            this.quickSort(arr, low, (pivot - 1)); 
+            this.quickSort(arr, (pivot + 1), high); 
+        } 
+        else {
+            //Do nothing
+        }
+    } 
 }
 
 module.exports = Sort;
